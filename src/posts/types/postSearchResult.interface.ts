2b81da1f5e11d3daa2299b2d1a5da3b08dc42561
Renolation <vodanh.2901@gmail.com>
2025-05-22 03:51:58 +0700

    aa

---
+++ src/posts/types/postSearchResult.interface.ts
@@ -0,0 +1,9 @@
+
+export interface PostSearchResult {
+  hits: {
+    total: number;
+    hits: Array<{
+      _source: PostSearchBody;
+    }>;
+  };
+}
---
+++ src/search/search.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
+
+@Module({
+  imports: [
+    ConfigModule,
+    ElasticsearchModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        node: configService.get<string>('ELASTICSEARCH_NODE'),
+        auth: {
+          username: configService.get<string>('ELASTICSEARCH_USERNAME'),
+          password: configService.get<string>('ELASTICSEARCH_PASSWORD'),
+        }
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [ElasticsearchModule]
+})
+export class SearchModule {}
